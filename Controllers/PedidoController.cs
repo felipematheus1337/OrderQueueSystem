1fb93d15b273739a8824d0ccb1c38d87a12b8a0b
felipematheus1337 <lipehbr@gmail.com>
2025-05-19 16:03:01 -0300

    criado controller.

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrderQueueSystem.Dtos;
+using OrderQueueSystem.Services;
+
+namespace OrderQueueSystem.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class PedidoController : ControllerBase
+{
+
+    private readonly PedidoService _service;
+
+    public PedidoController (PedidoService pedidoService)
+    {
+        _service = pedidoService;
+    }
+
+    [HttpGet("{id:int:min(1)", Name = "ObterPedido")]
+    public async Task<ActionResult<PedidoResponseDto>> buscar(int id)
+    {
+        var pedido = await _service.BuscarPorId(id);
+
+        if (pedido is null) return NotFound();
+
+        return Ok(pedido);
+
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CriarPedido([FromBody] PedidoRequestDto dto)
+    {
+        if (dto is null) return BadRequest();
+
+        await _service.CriarPedido(dto);
+
+        return Created();
+
+    }
+    
+
+    
+
+}
